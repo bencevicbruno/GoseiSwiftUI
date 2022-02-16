8eab0047b423cec5e4a2f837aad8d03afe8775d2
bruno-bencevic <bruno.bencevic@cobeisfresh.com>
2022-02-16 10:23:49 +0100

    Init

---
+++ GoseiSwiftUI/GoseiSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoseiSwiftUIApp.swift
+//  GoseiSwiftUI
+//
+//  Created by Bruno Benčević on 16.02.2022..
+//
+
+import SwiftUI
+
+@main
+struct GoseiSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
